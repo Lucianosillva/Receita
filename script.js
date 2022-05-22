8145aa33c124c5482f0a727c67f45f9a51caea17
Lucianosillva <motoe2udi@gmail.com>
2022-05-22 07:45:31 -0300

    Ajustes no estilo e responsividade do site

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-/*As duas funções abaixo alternam entre 'ingredientes' e 'modo de preparo' em telas pequenas. */
+/*As duas funções abaixo alternam entre 'ingredientes' e 'modo de preparo' em telas pequenas, e também muda a cor dos botões. */
 function mododepreparo() {
     document.getElementById('ingredientescelular').style.display = 'none'
     document.getElementById('mododepreparocelular').style.display = 'block'
+    document.getElementById('modo').style.fontWeight= 'bolder'
+    document.getElementById('modo').style.color= 'var(--cor1)'
+    document.getElementById('ing').style.fontWeight= 'normal'
+    document.getElementById('ing').style.color= 'rgba(0, 0, 0, 0.473)'
 }
 function ingredientes() {
     document.getElementById('mododepreparocelular').style.display = 'none'
     document.getElementById('ingredientescelular').style.display = 'block'
+    document.getElementById('ing').style.fontWeight= 'bolder'
+    document.getElementById('ing').style.color= 'var(--cor1)'
+    document.getElementById('modo').style.fontWeight= 'normal'
+    document.getElementById('modo').style.color= 'rgba(0, 0, 0, 0.473)'
 }
